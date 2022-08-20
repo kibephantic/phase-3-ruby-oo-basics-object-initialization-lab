150c0fe3d74c00968f5b341f000134a4c53a1109
PatrickKibe <phantickibe01@gmail.com>
2022-08-20 23:36:28 +0300

    commit tree bcfc74652446e954664985b0d3648f6dcdf12ba6

--- spec/object_initialize_spec.rb
+++ spec/object_initialize_spec.rb
@@ -12,7 +12,6 @@ describe 'Dog' do
   describe '#initialize' do
     it 'sets the name of the dog in an instance variable @name' do
       fido = Dog.new("Fido", "Pug")
-
       expect(fido.instance_variable_get(:@name)).to eq("Fido")
     end
 
